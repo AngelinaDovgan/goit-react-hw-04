859408ece7d44be2b936788d1b2c062c5bb80d51
Angelina <midnightshout03@gmail.com>
2024-04-18 12:58:54 +0300

    5

--- src/gallery.js
+++ src/gallery.js
@@ -1,11 +1,10 @@
 import axios from "axios";
 
 axios.defaults.baseURL = "https://api.unsplash.com/";
-const AccessKey = "G2AovgaDubgxBcNqcyg3uSWnupRNVD8P1HEpklKMohw";
+const KEY = "G2AovgaDubgxBcNqcyg3uSWnupRNVD8P1HEpklKMohw";
 
-export const fetchGallery = async (searchQuery) => {
+export const fetchGallery = async (searchQuery, page) => {
     
-    const response = await axios.get(`/search/photos/?client_id=${access}&query=${searchQuery}`); 
-    console.log(response.data.results);
+    const response = await axios.get(`/search/photos/?query=${searchQuery}&client_id=${KEY}&per_page=10&page=${page}`); 
     return response.data.hits;
 }
